f6df5d0fed5cdf53494058506294b2eaa2eac43e
xutongle <xutongle@msn.com>
2026-02-25 19:45:56 +0800

    feat(ci): 添加云原生开发环境配置和Dockerfile
    docs: 更新短信网关配置文档
    refactor: 重构短信网关配置结构

--- config/sms.php
+++ config/sms.php
@@ -35,20 +35,37 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Log Gateways
+    | SMS Gateways
     |--------------------------------------------------------------------------
     |
     | Here you may configure the sms gateways for your application.  This gives
     | you a variety of powerful log handlers / formatters to utilize.
     |
-    | Available Drivers: "aliyun", "alidayu", "yunpian", "submail",
+    | Available Gateways: "aliyun", "alidayu", "yunpian", "submail",
     |                    "errorlog", "luosimao", "yuntongxun", "huyi"
     |                    "juhe", "sendcloud", "baidu", "huaxin", "chuanglan"
     |                    "rongcloud", "tianyiwuxian", "twilio", "qcloud", "avatardata"
+    | 更多配置 https://github.com/overtrue/easy-sms
     |
     */
     'gateways' => [
-        //Doc
-        // https://github.com/overtrue/easy-sms
+        'aliyun' => [
+            'access_key_id' => '',
+            'access_key_secret' => '',
+            'sign_name' => '',
+        ],
+        'qcloud' => [
+            'sdk_app_id' => '', // 短信应用的 SDK APP ID
+            'secret_id' => '', // SECRET ID
+            'secret_key' => '', // SECRET KEY
+            'sign_name' => '腾讯CoDesign', // 短信签名
+        ],
+        'volcengine' => [
+            'access_key_id' => '', // 平台分配给用户的access_key_id
+            'access_key_secret' => '', // 平台分配给用户的access_key_secret
+            'region_id' => 'cn-north-1', // 国内节点 cn-north-1，国外节点 ap-singapore-1，不填或填错，默认使用国内节点
+            'sign_name' => '', // 平台上申请的接口短信签名或者签名ID，可不填，发送短信时data中指定
+            'sms_account' => '', // 消息组帐号,火山短信页面右上角，短信应用括号中的字符串，可不填，发送短信时data中指定
+        ],
     ],
 ];
