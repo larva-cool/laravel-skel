e3bd59f567cedf2f442a0d9c1d038649b145354f
xutongle <xutongle@msn.com>
2026-03-13 19:50:34 +0800

    feat(数据库): 添加导入昵称数据的迁移文件
    添加新的数据库迁移文件用于从JSON文件批量导入昵称数据
    在非测试环境下运行时，会分批处理数据以避免内存问题

--- database/migrations/2026_02_25_194126_import_nickname_data.php
+++ database/migrations/2026_02_25_194126_import_nickname_data.php
@@ -19,21 +19,18 @@ return new class extends Migration
     {
         if (! app()->runningUnitTests()) {// testing 跳过
             ini_set('memory_limit', '-1');
-            // 写入随机昵称
-            $data = FileHelper::json(database_path('data/nickname-20251129.json'));
+            $items = FileHelper::json(database_path('data/nickname-20251129.json'));
             $nicknames = [];
-            foreach ($data as $key => $val) {
+            foreach ($items as $key => $val) {
                 $nicknames[] = ['nickname' => $val];
-                // 1000个一组写入数据库
-                if ($key % 1000 === 0) {
+                // 5000个一组写入数据库
+                if ($key % 5000 === 0) {
                     Nickname::insert($nicknames);
                     $nicknames = [];
                 }
             }
             Nickname::insert($nicknames);
-            // 释放内存
-            $data = $nicknames = null;
-            unset($data, $nicknames);
+            unset($items, $nicknames);
         }
     }
 
