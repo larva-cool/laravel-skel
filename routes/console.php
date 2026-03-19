456be7ac52ff06ae9d06a4bb7b1c0f1fdc596a0d
xutongle <xutongle@msn.com>
2026-03-19 15:41:52 +0800

    feat: 添加模型和迁移模板文件并更新控制台路由
    添加模型模板文件(model.stub)和迁移模板文件(migration.create.stub)
    在控制台路由中添加定时任务配置，包括模型清理、队列健康检查和开发环境下的Telescope清理

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,24 @@
 <?php
+/**
+ * This is NOT a freeware, use is subject to license terms.
+ */
+
+declare(strict_types=1);
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// 清理模型
+Schedule::command('model:prune')->daily();
+
+// 队列健康指标
+Schedule::command('horizon:snapshot')->everyFiveMinutes();
+
+if (! app()->isProduction()) {
+    Schedule::command('telescope:prune --hours=24')->daily();
+}
